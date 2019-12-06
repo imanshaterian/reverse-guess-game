8226a2b1b7ecb7336784366509ada07c1d544308
Iman Shaterian <imanshaterian@gmail.com>
2019-12-07 00:02:49 +0330

    making some adjustments

--- GameCode.py
+++ GameCode.py
@@ -1,10 +1,11 @@
 # make a max and min for making the possible area smaler by every step
 MaxNumber = 100
 MinNumber = 0
-print('Hi .This is a guess game. You choose a number between 1 and 100 and i should try to find it.\n
-if your number is SMALLER than my guess press "l".\n
-if your number is BIGGER than my guess press "h".\n
-if i guessed the number right press "y".\n')
+print('''Hi .This is a guess game.
+You choose a number between 1 and 100 and i should try to find it.
+if your number is SMALLER than my guess press "l".
+if your number is BIGGER than my guess press "h".
+if i guessed the number right press "y".''')
 for i in range(0, 100):
     # average is the answer that will be printed in every step
     Average = int((MaxNumber + MinNumber)/2)
@@ -21,3 +22,4 @@ for i in range(0, 100):
         break
     else:
         print('please enter the correct character X( :\n')
+
